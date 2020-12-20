8b032ba6c33de6899cd586bef799684ac9b8fdb3
pavlo.bokhan <pavlo.bokhan@jointoit.com>
2020-12-20 18:58:37 +0200

    next.js

---
+++ _next/static/QWJZq1s3oZ-I48cdhCk5G/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-e69146ff375df505c2d4.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-52db11f997c1ca02bd51.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
